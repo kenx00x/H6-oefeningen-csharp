5ead38133de142c12306051348a47f158f6e8965
kenx00x <kenx00x@gmail.com>
2020-11-19 23:29:08 +0100

    code cleanup

--- Opwarmers van opwarmers/Program.cs
+++ Opwarmers van opwarmers/Program.cs
@@ -61,7 +61,7 @@ namespace Opwarmers_van_opwarmers
                 }
             } while (n != -32768);
             Console.WriteLine($"postieve = {uitkomstpos}");
-            Console.WriteLine($"negatieve = {uitkomstneg-1}");
+            Console.WriteLine($"negatieve = {uitkomstneg - 1}");
             Console.WriteLine($"nullen = {uitkomstnul}");
             Console.WriteLine();
 
@@ -72,7 +72,7 @@ namespace Opwarmers_van_opwarmers
             {
                 Console.Write("Geef getal in(0 om te stoppen): ");
                 n = int.Parse(Console.ReadLine());
-                if (n !=0)
+                if (n != 0)
                 {
                     uitkomst *= n;
                 }
@@ -96,19 +96,19 @@ namespace Opwarmers_van_opwarmers
             //6
             Console.WriteLine("6");
             n = 0;
-            int aantal=0;
-            decimal gemiddelde=0;
+            int aantal = 0;
+            decimal gemiddelde = 0;
             do
             {
                 Console.Write("Geef getal in(negatief getal om af te sluiten): ");
                 aantal++;
                 n = int.Parse(Console.ReadLine());
-                if (n>=0)
+                if (n >= 0)
                 {
                     gemiddelde += n;
                 }
-            } while (n>=0);
-            Console.WriteLine($"Gemiddelde = {Math.Round(gemiddelde/Convert.ToDecimal(aantal),0)}");
+            } while (n >= 0);
+            Console.WriteLine($"Gemiddelde = {Math.Round(gemiddelde / Convert.ToDecimal(aantal), 0)}");
             Console.WriteLine();
 
             //7
@@ -119,7 +119,7 @@ namespace Opwarmers_van_opwarmers
             {
                 Console.Write("Geef getal in(-32768 om te stoppen): ");
                 n = int.Parse(Console.ReadLine());
-                if ((strKleinsteGetal == "" || n<int.Parse(strKleinsteGetal) )&& n != -32768)
+                if ((strKleinsteGetal == "" || n < int.Parse(strKleinsteGetal)) && n != -32768)
                 {
                     strKleinsteGetal = Convert.ToString(n);
                 }
