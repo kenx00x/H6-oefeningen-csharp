13c351efe41236eaefb42c46058225d5e6d600bf
kenx00x <kenx00x@gmail.com>
2020-11-19 13:25:46 +0100

    oefeningen

--- Opwarmers 1/Program.cs
+++ Opwarmers 1/Program.cs
@@ -6,6 +6,7 @@ namespace Opwarmers_1
     {
         static void Main(string[] args)
         {
+            //Toon alle natuurlijke getallen van 1 tot n.
             Console.Write("Geef n: ");
             int n = int.Parse(Console.ReadLine());
             for (int i = 1; i < n+1; i++)
@@ -14,6 +15,7 @@ namespace Opwarmers_1
             }
             Console.WriteLine();
 
+            //Toon alle natuurlijke getallen van n tot 1.
             Console.Write("Geef n: ");
             for (n = int.Parse(Console.ReadLine()); n < 1 + 1; n++)
             {
@@ -21,18 +23,21 @@ namespace Opwarmers_1
             }
             Console.WriteLine();
 
+            //Toon alle even getallen tussen 1 en 100.
             for (int i = 1; i < 101; i+=2)
             {
                 Console.WriteLine(i);
             }
             Console.WriteLine();
 
+            //Toon alle oneven getallen tussen 1 en 100.
             for (int i = 2; i < 101; i += 2)
             {
                 Console.WriteLine(i);
             }
             Console.WriteLine();
 
+            //Toon de som van alle getallen van 1 tot n (dus 1+2+3+4+...+n).
             int j = 0;
             Console.Write("Geef n: ");
             n = int.Parse(Console.ReadLine());
@@ -43,6 +48,7 @@ namespace Opwarmers_1
             Console.WriteLine(j);
             Console.WriteLine();
 
+            //Toon de som van alle even getallen van 1 tot n.
             j = 0;
             Console.Write("Geef n: ");
             n = int.Parse(Console.ReadLine());
@@ -53,6 +59,7 @@ namespace Opwarmers_1
             Console.WriteLine(j);
             Console.WriteLine();
 
+            //Toon de som van alle oneven getallen van 1 tot n.
             j = 0;
             Console.Write("Geef n: ");
             n = int.Parse(Console.ReadLine());
---
+++ Opwarmers van opwarmers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Opwarmers_van_opwarmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            int n;
+            int uitkomst=0;
+            do
+            {
+                Console.Write("Geef getal in(0 om te stoppen): ");
+                n = int.Parse(Console.ReadLine());
+                uitkomst += n;
+            } while (n != 0);
+            Console.WriteLine($"som = {uitkomst}");
+
+            //2
+            int uitkomstpos = 0;
+            int uitkomstneg = 0;
+            do
+            {
+                Console.Write("Geef getal in(0 om te stoppen): ");
+                n = int.Parse(Console.ReadLine());
+                if (n > 0)
+                {
+                    uitkomstpos++;
+                }
+                else if (n < 0)
+                {
+                    uitkomstneg++;
+                }
+            } while (n != 0);
+            Console.WriteLine($"postieve = { uitkomstpos}");
+            Console.WriteLine($"negatieve = { uitkomstneg}");
+
+            //3
+            int uitkomstnul = 0;
+            do
+            {
+                Console.Write("Geef getal in(-32768 om te stoppen): ");
+                n = int.Parse(Console.ReadLine());
+                if (n > 0)
+                {
+                    uitkomstpos++;
+                }
+                else if (n < 0)
+                {
+                    uitkomstneg++;
+                }
+                else if (n==0)
+                {
+                    uitkomstnul++;
+                }
+            } while (n != -32768);
+            Console.WriteLine($"postieve = { uitkomstpos}");
+            Console.WriteLine($"negatieve = { uitkomstneg}");
+            Console.WriteLine($"nullen = { uitkomstnul}");
+        }
+    }
+}
