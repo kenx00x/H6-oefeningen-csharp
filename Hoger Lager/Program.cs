c7b0e32c6061d92fbc58b6fc3def575e31edbc94
kenx00x <kenx00x@gmail.com>
2020-11-26 13:31:49 +0100

    Hoger Lager

---
+++ Hoger Lager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Hoger_Lager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int getal;
+            int randomGetal = random.Next(1, 101);
+            do
+            {
+                Console.Write("Kies een getal: ");
+                getal = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (randomGetal > getal)
+                {
+                    Console.WriteLine("Hoger");
+                }
+                else if (randomGetal < getal)
+                {
+                    Console.WriteLine("Lager");
+                }
+                else if (randomGetal == getal)
+                {
+                    Console.WriteLine("Correct!");
+                }
+            } while (getal != randomGetal);
+        }
+    }
+}
